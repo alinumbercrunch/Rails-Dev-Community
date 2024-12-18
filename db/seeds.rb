5aae44271de74e21ea2ec74c8e90e5b6ee0bce59
ali haddi <alij2297@hotmail.com>
2024-12-18 20:19:37 +0900

    Add:faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+ActiveRecord::Base.transaction do
+  begin
+    10.times do |i|
+      user = User.create!(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        username: "#{Faker::Name.first_name.downcase}_#{i+10}",
+        email: Faker::Internet.email,
+        contact_number: Faker::PhoneNumber.phone_number_with_country_code,
+        city: Faker::Address.city,
+        state: Faker::Address.state,
+        country: Faker::Address.country,
+        pincode: Faker::Address.zip_code,
+        date_of_birth: (Date.today - rand(18..65).years),
+        profile_title: Faker::Job.title, # Assuming PROFILE_TITLE is an array of job titles
+        password: "123456",
+        about: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia."
+      )
+      puts "User #{i+1} created successfully"
+    end
+  rescue ActiveRecord::RecordInvalid => e
+    puts "Transaction failed: #{e.message}"
+    raise ActiveRecord::Rollback
+  end
+end
